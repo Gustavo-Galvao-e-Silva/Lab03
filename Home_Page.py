90f817be0078c9161af4e2ffdc08abcf7c587bb5
gustavogalvao <gugagalvao@live.com>
2025-10-27 19:02:18 -0400

    First commit

---
+++ Home_Page.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Web Development Lab03")
+
+st.header("CS 1301")
+st.subheader("Team 43, Web Development - Section A")
+st.subheader("Gustavo Galvao e Silva, Team Member 2")
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! Our app (INSERT WELCOME MESSAGE). You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Home Page: This is the page you are currently on! Here, you can find general information about our project and its pages
+2.
+3.
+4.
+
+""")
+
